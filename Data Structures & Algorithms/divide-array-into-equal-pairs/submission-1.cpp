6d174cb127a0c4c244303e1e4bb5f913201e8018
alpinebeast649 <mehtadhyey21@gmail.com>
2026-08-21 21:06:31 +0100

    Add: divide-array-into-equal-pairs - submission-1

---
+++ Data Structures & Algorithms/divide-array-into-equal-pairs/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for(int num : nums) {
+            freq[num]++;
+        }
+
+        for(const auto&pair: freq) {
+            if(pair.second % 2 == 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
