86402a4357c71516530345f6cc92199608a95c07
alpinebeast649 <mehtadhyey21@gmail.com>
2026-08-22 23:45:54 +0100

    Update: unique-email-addresses - submission-1

--- Data Structures & Algorithms/unique-email-addresses/submission-1.cpp
+++ Data Structures & Algorithms/unique-email-addresses/submission-1.cpp
@@ -1,17 +1,41 @@
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
-        int count = 0;
-
-        int n = nums.size();
-
-        for(int i = 0; i < n; i++) {
-            if(nums[i] != val) {
-                nums[count] = nums[i];
-                count++;
+    int numUniqueEmails(vector<string>& emails) {
+        int length = emails.size();
+        unordered_set<string>em;
+        string email = "";
+        for(int i = 0; i < length; i++) {
+            email = emailConverter(emails[i]);
+            //cout << email << endl;
+            if(em.count(email)) {
+                continue;
+            }
+            em.insert(email);
+        }
+        return em.size();
+    }
+    string emailConverter(string email) {
+        string ans = "";
+        int len = email.length();
+        int at = 0;
+        for(int i = 0; i < len; i++) {
+            if(email[i] == '@') {
+                at = i;
+                break;
             }
         }
-
-        return count;
+        for(int i = 0; i < at; i++) {
+            if(email[i] == '.') {
+                ans = ans;
+            } else if (email[i] != '+') {
+                ans += email[i];
+            } else {
+                break;
+            }
+        }
+        for(int j = at; j < len; j++) {
+            ans += email[j];
+        }
+        return ans;
     }
 };
